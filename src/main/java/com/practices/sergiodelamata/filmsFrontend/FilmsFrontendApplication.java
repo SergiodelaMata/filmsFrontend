4de628cbb1b918f0314f247361606399ca125fa3
SergiodelaMata <sergio.matam@edu.uah.es>
2022-01-21 12:18:52 +0100

    :sparkles: Added structure from Spring Boot Framework.

---
+++ src/main/java/com/practices/sergiodelamata/filmsFrontend/FilmsFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.practices.sergiodelamata.filmsFrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmsFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmsFrontendApplication.class, args);
+	}
+
+}
