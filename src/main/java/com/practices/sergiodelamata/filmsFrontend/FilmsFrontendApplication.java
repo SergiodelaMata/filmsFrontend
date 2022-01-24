ee4f91851c972d3b62218f6c90391b3fdac681bc
SergiodelaMata <sergio.matam@edu.uah.es>
2022-01-24 23:58:22 +0100

    :sparkles: Established the templates and the connections needed to use the microservice for frontend view.

--- src/main/java/com/practices/sergiodelamata/filmsFrontend/FilmsFrontendApplication.java
+++ src/main/java/com/practices/sergiodelamata/filmsFrontend/FilmsFrontendApplication.java
@@ -2,12 +2,22 @@ package com.practices.sergiodelamata.filmsFrontend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class FilmsFrontendApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(FilmsFrontendApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate template()
+	{
+		RestTemplate template = new RestTemplate();
+		return template;
 	}
 
 }
