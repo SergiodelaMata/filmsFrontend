560df1ab447fe9a8b50192a6dc47584e4a7afd12
SergiodelaMata <sergio.matam@edu.uah.es>
2022-01-30 00:44:57 +0100

    :sparkles: Added condition to check that the minimum fields required are being set before inserting or update the data.

--- src/main/resources/static/js/newActor.js
+++ src/main/resources/static/js/newActor.js
@@ -4,10 +4,22 @@ window.addEventListener("DOMContentLoaded", function() {
     formActor = document.getElementById("form-actor");
     formActor.addEventListener("submit", async(event) =>{
         event.preventDefault();
-        const formData = formSerializer(event);
-        await(postRequest("/actors/save",formData));
-        //const response = await(postRequest("/actors/save",formData));
-        alert("Los datos del actor se han guardado correctamente.");
-        window.location.href = "/actors";
+        const name = document.getElementById("name").value;
+        const birthdate = document.getElementById("birthDate").value;
+        const countryBirth = document.getElementById("countryBirth").value;
+        if(name != null && name !== "" && birthdate != null && birthdate !== ""
+            && countryBirth != null && countryBirth !== "")
+        {
+            const formData = formSerializer(event);
+            await(postRequest("/actors/save",formData));
+            //const response = await(postRequest("/actors/save",formData));
+            alert("Los datos del actor se han guardado correctamente.");
+            window.location.href = "/actors";
+        }
+        else
+        {
+            alert("Algunos de los campos obligatorios del formulario no se ha rellenado. " +
+                "Por favor rellénelos para poder insertarlo.");
+        }
     });
 }, false);
--- src/main/resources/static/js/updateActor.js
+++ src/main/resources/static/js/updateActor.js
@@ -4,11 +4,22 @@ window.addEventListener("DOMContentLoaded", function() {
     formActor = document.getElementById("form-actor");
     formActor.addEventListener("submit", async(event) =>{
         event.preventDefault();
-        const formData = formSerializer(event);
-        //console.log(formData);
-        await(putRequest("/actors/save",formData));
-        //const response = await(putRequest("/films/save",formData));
-        alert("Los datos del actor se han guardado correctamente.");
-        window.location.href = "/actors";
+        const name = document.getElementById("name").value;
+        const birthdate = document.getElementById("birthDate").value;
+        const countryBirth = document.getElementById("countryBirth").value;
+        if(name != null && name !== "" && birthdate != null && birthdate !== ""
+            && countryBirth != null && countryBirth !== "")
+        {
+            const formData = formSerializer(event);
+            await (putRequest("/actors/save", formData));
+            //const response = await(putRequest("/films/save",formData));
+            alert("Los datos del actor se han guardado correctamente.");
+            window.location.href = "/actors";
+        }
+        else
+        {
+            alert("Algunos de los campos obligatorios del formulario se han vaciado. " +
+                "Por favor rellénelos para poder actualizarlos.");
+        }
     });
 }, false);
