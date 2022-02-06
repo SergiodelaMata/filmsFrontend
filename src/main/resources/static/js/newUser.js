f40f42d29f459a32c4e28c37b09d79016ee9c7f8
SergiodelaMata <sergio.matam@edu.uah.es>
2022-02-06 14:22:41 +0100

    :sparkles: Introduced verify password field.

--- src/main/resources/static/js/newUser.js
+++ src/main/resources/static/js/newUser.js
@@ -4,10 +4,19 @@ window.addEventListener("DOMContentLoaded", function() {
     formUser = document.getElementById("form-user");
     formUser.addEventListener("submit", async(event) =>{
         event.preventDefault();
-        const formData = formSerializer(event);
-        await(postRequest("/users/save",formData));
-        //const response = await(postRequest("/users/save",formData));
-        alert("Los datos del usuario se han guardado correctamente.");
-        window.location.href = "/users";
+        const password = document.getElementById("password").value;
+        const verifyPassword = document.getElementById("verifyPassword").value;
+        if(password === verifyPassword)
+        {
+            const formData = formSerializer(event);
+            await(postRequest("/users/save",formData));
+            //const response = await(postRequest("/users/save",formData));
+            alert("Los datos del usuario se han guardado correctamente.");
+            window.location.href = "/users";
+        }
+        else
+        {
+            alert("No coinciden las contraseñas. Por favor introduzcalas para que coincidan.")
+        }
     });
 }, false);
