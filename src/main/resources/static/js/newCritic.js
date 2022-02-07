35b178853835254e02f7e8024c725f6c0ea38e03
SergiodelaMata <sergio.matam@edu.uah.es>
2022-02-07 23:59:45 +0100

    :bug: Corrected error which made that always was taken the first option when inserting a new critic from the critics section.

--- src/main/resources/static/js/newCritic.js
+++ src/main/resources/static/js/newCritic.js
@@ -3,11 +3,12 @@ var formCritic = document.getElementById("form-critic");
 window.addEventListener("DOMContentLoaded", function() {
     formCritic = document.getElementById("form-critic");
     const selectFilm = document.getElementById("selectFilms");
-    const value = getSelectValues(selectFilm);
+    var value = getSelectValues(selectFilm);
 
     formCritic.addEventListener("submit", async(event) =>{
         event.preventDefault();
         const formData = formSerializer(event);
+        value = getSelectValues(selectFilm);
         await(postRequest("/critics/save/" + value[0],formData));
         alert("Los datos de la crítica se han guardado correctamente.");
         window.location.href = "/critics";
